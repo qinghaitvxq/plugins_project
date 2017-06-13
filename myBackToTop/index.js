9459b0f900ebd32e61934f04d74b8bf9b3c20246
杨杰斌 <yangjb@chinanetcenter.com>
2017-06-13 15:48:52 +0800

    add jquery plugins

--- myBackToTop/index.js
+++ myBackToTop/index.js
@@ -1,31 +1,65 @@
 /**
  * Created by caipf on 2017/6/12.
  */
-class BackToTop{
-    constructor(options){
-        let defaultOptions={
-            element:''
+// class BackToTop{
+//     constructor(options){
+//         let defaultOptions={
+//             element:''
+//         }
+//         this.options=Object.assign({},defaultOptions,options);
+//         this.checkOptions().bindEvent();
+//     }
+//     checkOptions(){
+//         if(!this.options.element){
+//             let div=document.createElement("div");
+//             div.setAttribute("class","backToTop");
+//             div.setAttribute("id","backToTop");
+//             document.querySelector("body").appendChild(div);
+//             this.options.element=div;
+//         }
+//         return this;
+//     }
+//     bindEvent(){
+//        this.options.element.addEventListener('click',(e)=>{
+//            alert('hello world');
+//        });
+//        return this;
+//     }
+// }
+
+(function ($) {
+    class BackToTop{
+        constructor(options){
+            let defaultOptions={
+                element:''
+            }
+            this.options=Object.assign({},defaultOptions,options);
+            this.checkOptions().bindEvent();
         }
-        this.options=Object.assign({},defaultOptions,options);
-        this.checkOptions().bindEvent();
-    }
-    checkOptions(){
-        if(!this.options.element){
-            let div=document.createElement("div");
-            div.setAttribute("class","backToTop");
-            div.setAttribute("id","backToTop");
-            document.querySelector("body").appendChild(div);
-            this.options.element=div;
+        checkOptions(){
+            if(!this.options.element){
+                let div=document.createElement("div");
+                div.setAttribute("class","backToTop");
+                div.setAttribute("id","backToTop");
+                document.querySelector("body").appendChild(div);
+                this.options.element=div;
+            }
+            return this;
         }
-        return this;
+        bindEvent(){
+            this.options.element.addEventListener('click',(e)=>{
+                alert('hello world');
+            });
+            return this;
+        }
     }
-    bindEvent(){
-       this.options.element.addEventListener('click',(e)=>{
-           alert('hello world');
-       });
-       return this;
+    $.fn.goBackToTop=function () {
+        var options={};
+        options.element=this.get(0);
+        new BackToTop(options);
     }
-}
+})(jQuery);
+
 // 1.获取页面可视区的高度
 // clientHeight=document.documentElement.clientHeight;
 // 2.获取滚动条距离顶部的高度
